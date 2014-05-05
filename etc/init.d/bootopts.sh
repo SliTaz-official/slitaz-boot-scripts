7e13e26b53c2ef26569e405a7bba1acedd1ef316
Christophe Lincoln <pankso@slitaz.org>
2014-05-05 22:02:43 +0200

    Add user to disk group... we never know!

--- etc/init.d/bootopts.sh
+++ etc/init.d/bootopts.sh
@@ -43,7 +43,7 @@ if ! grep -q "100[0-9]:100" /etc/passwd; then
 	echo -n "Configuring user and group: $USER..."
 	adduser -D -s /bin/sh -g "SliTaz User" -G users -h /home/$USER $USER
 	passwd -d $USER >/dev/null
-	for group in audio cdrom video tty plugdev
+	for group in audio cdrom video tty plugdev disk
 	do
 		addgroup $USER ${group}
 	done
