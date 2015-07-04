7cce34ebfbf703bedcd33c5218d209fd0b9c60e4
Pascal Bellard <pascal.bellard@slitaz.org>
2015-07-04 18:41:59 +0200

    wpa_action.sh: typo

--- etc/init.d/wpa_action.sh
+++ etc/init.d/wpa_action.sh
@@ -3,7 +3,7 @@ INTERFACE=$1
 
 # For a dynamic IP with DHCP.
 if [ "$2" = "CONNECTED" ]; then
-	[ -f /var/run/udhcpc.$INTERFACE.pid] && killall udhcpc
+	[ -f /var/run/udhcpc.$INTERFACE.pid ] && killall udhcpc
 	/sbin/udhcpc -b -i $INTERFACE -p /var/run/udhcpc.$INTERFACE.pid
 elif [ "$2" = "DISCONNECTED" ]; then
 	/sbin/udhcpc -b -i $INTERFACE -p /var/run/udhcpc.$INTERFACE.pid
